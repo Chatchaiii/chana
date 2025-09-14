ae6d169c171d83ad1e606c781b778b289831468c
Chatchaiii <chatchaik.bozkir@gmail.com>
2025-09-15 01:35:29 +0200

    2025/09/15

--- src/components/PasswordProtection.tsx
+++ src/components/PasswordProtection.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { CircleArrowRight } from "lucide-react";
 
 interface PasswordProtectionProps {
   onBack: () => void;
@@ -10,16 +11,16 @@ export function PasswordProtection({ onBack }: PasswordProtectionProps) {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const correctPassword = "zendegim"; // Change this to your desired password
+  const correctPassword = "zendegim";
 
   if (!entered) {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
         <div className="bg-gray-800 p-8 rounded-2xl shadow-lg flex flex-col items-center">
-          <h2 className="text-xl font-bold text-gray-200 mb-4">Enter Password</h2>
+          {/* <h2 className="text-xl font-bold text-gray-300 mb-4">Enter Password</h2> */}
           <input
             type="password"
-            className="p-2 rounded bg-gray-700 text-gray-200 mb-2 outline-none"
+            className="p-2 rounded bg-gray-800 text-gray-200 mb-2 outline-none"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             onKeyDown={(e) => {
@@ -29,27 +30,29 @@ export function PasswordProtection({ onBack }: PasswordProtectionProps) {
                   setEntered(true);
                   setError("");
                 } else {
-                  setError("Incorrect password");
+                  setError("no-possible");
                 }
               }
             }}
-            placeholder="Password"
+            placeholder="password"
           />
           <button
-            className="bg-pink-600 text-white px-4 py-2 rounded font-bold"
+            className="bg-transparent text-gray-300 px-4 py-1 rounded-full font-bold"
             onClick={() => {
               if (password === correctPassword) {
                 onBack();
                 setEntered(true);
                 setError("");
               } else {
-                setError("Incorrect password");
+                setError("No-possible");
               }
             }}
           >
-            Submit
+            <CircleArrowRight
+              className="text-gray-400"
+            />
           </button>
-          {error && <div className="text-red-500 mt-2">{error}</div>}
+          {error && <div className="text-red-300 mt-2">{error}</div>}
         </div>
       </div>
     );
